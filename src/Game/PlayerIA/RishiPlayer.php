0cd507c6738cdb07ecc712563da4c9ffb1763d6f
Louise Delduc <louise.delduc@epita.fr>
2020-10-07 11:04:28 -0700

    add first algo structure

--- src/Game/PlayerIA/RishiPlayer.php
+++ src/Game/PlayerIA/RishiPlayer.php
@@ -3,11 +3,12 @@
 namespace Hackathon\PlayerIA;
 
 use Hackathon\Game\Result;
+use function Sodium\library_version_major;
 
 /**
  * Class RishiPlayers
  * @package Hackathon\PlayerIA
- * @author YOUR NAME HERE
+ * @author DELDUC Louise
  */
 class RishiPlayer extends Player
 {
@@ -17,32 +18,26 @@ class RishiPlayer extends Player
 
     public function getChoice()
     {
-        // -------------------------------------    -----------------------------------------------------
-        // How to get my Last Choice           ?    $this->result->getLastChoiceFor($this->mySide) -- if 0 (first round)
-        // How to get the opponent Last Choice ?    $this->result->getLastChoiceFor($this->opponentSide) -- if 0 (first round)
-        // -------------------------------------    -----------------------------------------------------
-        // How to get my Last Score            ?    $this->result->getLastScoreFor($this->mySide) -- if 0 (first round)
-        // How to get the opponent Last Score  ?    $this->result->getLastScoreFor($this->opponentSide) -- if 0 (first round)
-        // -------------------------------------    -----------------------------------------------------
-        // How to get all the Choices          ?    $this->result->getChoicesFor($this->mySide)
-        // How to get the opponent Last Choice ?    $this->result->getChoicesFor($this->opponentSide)
-        // -------------------------------------    -----------------------------------------------------
-        // How to get my Last Score            ?    $this->result->getLastScoreFor($this->mySide)
-        // How to get the opponent Last Score  ?    $this->result->getLastScoreFor($this->opponentSide)
-        // -------------------------------------    -----------------------------------------------------
-        // How to get the stats                ?    $this->result->getStats()
-        // How to get the stats for me         ?    $this->result->getStatsFor($this->mySide)
-        //          array('name' => value, 'score' => value, 'friend' => value, 'foe' => value
-        // How to get the stats for the oppo   ?    $this->result->getStatsFor($this->opponentSide)
-        //          array('name' => value, 'score' => value, 'friend' => value, 'foe' => value
-        // -------------------------------------    -----------------------------------------------------
-        // How to get the number of round      ?    $this->result->getNbRound()
-        // -------------------------------------    -----------------------------------------------------
-        // How can i display the result of each round ? $this->prettyDisplay()
-        // -------------------------------------    -----------------------------------------------------
+        $nbRound = $this->result->getNbRound();
+        $opponent_choices = $this->result->getChoicesFor($this->opponentSide);
+        $nb_scissors = 0;
+        $nb_rocks = 0;
+        $nb_papers = 0;
 
+        foreach ($opponent_choices as &$choice) {
+            if ($choice == 'rock')
+                $nb_rocks += 1;
+            elseif ($choice == 'paper')
+                $nb_papers += 1;
+            else
+                $nb_scissors += 1;
+        }
 
-        return parent::rockChoice();
-
+        if ($nb_papers > $nb_scissors && $nb_papers > $nb_rocks)
+            return parent::paperChoice();
+        elseif ($nb_rocks > $nb_papers && $nb_rocks > $nb_scissors)
+            return parent::rockChoice();
+        else
+            return parent::scissorsChoice();
     }
 };
