f586f003bf3d905dbdddf380e480294e8619497e
Louise Delduc <louise.delduc@epita.fr>
2020-10-07 11:18:51 -0700

    other strategy

--- src/Game/PlayerIA/RishiPlayer.php
+++ src/Game/PlayerIA/RishiPlayer.php
@@ -18,26 +18,12 @@ class RishiPlayer extends Player
 
     public function getChoice()
     {
-        $nbRound = $this->result->getNbRound();
-        $opponent_choices = $this->result->getChoicesFor($this->opponentSide);
-        $nb_scissors = 0;
-        $nb_rocks = 0;
-        $nb_papers = 0;
-
-        foreach ($opponent_choices as &$choice) {
-            if ($choice == 'rock')
-                $nb_rocks += 1;
-            elseif ($choice == 'paper')
-                $nb_papers += 1;
-            else
-                $nb_scissors += 1;
-        }
-
-        if ($nb_papers > $nb_scissors && $nb_papers > $nb_rocks)
+        $mylast = $this->result->getLastChoiceFor($this->mySide);
+        if ($mylast == 'rock')
             return parent::paperChoice();
-        elseif ($nb_rocks > $nb_papers && $nb_rocks > $nb_scissors)
-            return parent::rockChoice();
-        else
+        elseif ($mylast == 'paper')
             return parent::scissorsChoice();
+        else
+            return parent::rockChoice();
     }
 };
